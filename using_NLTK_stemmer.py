93a2f687153d708ea8454710fc075f2c67febfa2
Wayne Burris <waterseeker@Hotmail.com>
2018-08-26 09:43:13 -0500

    added nltk stemmer package

---
+++ using_NLTK_stemmer.py
@@ -0,0 +1,9 @@
+"""
+
+To evaluate whether a tweet is 'good' or 'bad', we'll tokenize the tweet, and then stemmize each word in the tweet.
+We'll associate each stem with a positive or negative value using a dictionary.
+
+Finally, we'll calculate the average word weight of a tweet, and decide if it's a good or bad tweet based on that.
+
+"""
+
